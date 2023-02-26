630afb80c0d94e9bb3d1fadd2ced5ad5326211e9
loxygenK <me@loxygen.dev>
2023-02-27 00:49:07 +0900

    feat: add some validations

--- crates/shoutter_model/src/user.rs
+++ crates/shoutter_model/src/user.rs
@@ -1,7 +1,32 @@
+use validator::{Validate, ValidationError};
+
 use crate::id::Id;
 
+fn only_ascii(checking_str: &str) -> Result<(), ValidationError> {
+    if checking_str.is_ascii() {
+        Ok(())
+    } else {
+        Err(ValidationError::new("non_ascii_char_included"))
+    }
+}
+
+#[derive(Debug, Validate)]
 pub struct User {
     id: Id<Self>,
+    #[validate(
+        custom = "only_ascii",
+        length(min = 4, max = 32),
+    )]
+    screen_name: String,
+    #[validate(length(min = 4, max = 32))]
     name: String,
+    #[validate(length(min = 1, max = 512))]
     bio: String,
+}
+
+impl User {
+    pub fn new(id: Id<Self>, screen_name: String, name: String, bio: String) -> Self {
+        Self { id, screen_name, name, bio }
+    }
+
 }

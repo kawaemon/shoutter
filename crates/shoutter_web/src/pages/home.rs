75bc9068dc80b24b57e4256bd9e18e18a2801804
kawaemon <me@kawaemon.dev>
2023-03-24 01:16:42 +0900

    WIP

--- crates/shoutter_web/src/pages/home.rs
+++ crates/shoutter_web/src/pages/home.rs
@@ -1,11 +1,23 @@
+use diff_confused_rest_interface::{protobuf, GreetingEndpoint};
+use stylist::yew::use_style;
 use yew::{function_component, html, Html};
 use yew_router::prelude::use_navigator;
 
+use crate::hooks::api_client::{use_safe_endpoint, RequestState};
 use crate::pages::Route;
 
 #[function_component]
 pub fn Home() -> Html {
     let navigator = use_navigator().unwrap();
+    let greeting = use_safe_endpoint(
+        GreetingEndpoint,
+        (),
+        protobuf::GreetingName { name: "a".into() },
+    );
+    let code_style = use_style! {
+        display: block;
+        white-space: pre-wrap;
+    };
 
     html! {
         <>
@@ -13,6 +25,13 @@ pub fn Home() -> Html {
             <div>
                 <button onclick={move |_| navigator.push(&Route::Login)}>{"Login"}</button>
             </div>
+            <code class={code_style}>{
+                match &*greeting {
+                    RequestState::Pending => "Pending".to_owned(),
+                    RequestState::Ok(d) => d.content.clone(),
+                    RequestState::Err(e) => format!("Error:\n{e:?}")
+                }
+            }</code>
         </>
     }
 }
